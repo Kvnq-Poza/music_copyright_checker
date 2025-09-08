ce4ccdffe53a150f943c7635ed6f8167dbbad65d
kvnq-poza <kvnqpoza@gmail.com>
2025-09-08 19:39:15 +0100

    Add audio download page

--- frontend/src/app/components/sub/hero/hero.component.ts
+++ frontend/src/app/components/sub/hero/hero.component.ts
@@ -8,22 +8,34 @@ import { MusicDialogComponent } from '../../music-dialog/music-dialog.component'
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
 })
 export class HeroComponent {
-
-  constructor(private musicService: MusicCheckerService, private sanitizer: DomSanitizer, private dialog: MatDialog, private userService: SignupService, private renderer: Renderer2, private el: ElementRef) {}
-  musicItems: any[] = [];  // Array to hold music data
-  currentPage: number = 1;  // Current page for pagination
-  pageSize: number = 8;  // Items per page
-  totalPages: number = 0;  // Total pages for pagination
+  constructor(
+    private musicService: MusicCheckerService,
+    private sanitizer: DomSanitizer,
+    private dialog: MatDialog,
+    private userService: SignupService,
+    private renderer: Renderer2,
+    private el: ElementRef
+  ) {}
+  musicItems: any[] = []; // Array to hold music data
+  currentPage: number = 1; // Current page for pagination
+  pageSize: number = 9; // Items per page
+  totalPages: number = 0; // Total pages for pagination
   limit = 100;
-  tags: string[] = ['Background', 'Vlog Music', 'EDM', 'Gaming Music', 'Relax', 'Jazz'];
+  tags: string[] = [
+    'Background',
+    'Vlog Music',
+    'EDM',
+    'Gaming Music',
+    'Relax',
+    'Jazz',
+  ];
   selectedTag: string | null = '';
   filteredMusicItems: any[] = [];
-  likedMusicItems: any[] = []; 
+  likedMusicItems: any[] = [];
   isLogged: boolean = false;
-  
 
   ngOnInit() {
     // Mock data, replace with actual data fetch from an API or service
@@ -42,29 +54,25 @@ export class HeroComponent {
   }
 
   getMusics(): void {
-      this.musicService.getMusics(this.limit, this.currentPage).subscribe((response: any) => {
+    this.musicService
+      .getMusics(this.limit, this.currentPage)
+      .subscribe((response: any) => {
         this.musicItems = response;
         this.filteredMusicItems = this.musicItems;
         this.calculateTotalPages();
       });
+  }
+
+  getMusicsByUser(): void {
+    if (this.isLogged) {
+      this.musicService
+        .getMusicsByUser(this.limit, this.currentPage)
+        .subscribe((response: any) => {
+          this.likedMusicItems = response;
+          this.calculateTotalPages();
+        });
     }
-
-    getMusicsByUser(): void {
-        if (this.isLogged) {
-          this.musicService.getMusicsByUser(this.limit, this.currentPage).subscribe((response: any) => {
-            this.likedMusicItems = response;
-            this.calculateTotalPages();
-          });
-        }
-      }
-
-    
-
-   
-
-
-
-
+  }
 
   // Calculate the total number of pages based on the items
   calculateTotalPages() {
@@ -73,9 +81,11 @@ export class HeroComponent {
 
   // Get the items to display for the current page
   getPaginatedItems() {
-  
     const startIndex = (this.currentPage - 1) * this.pageSize;
-    const endIndex = Math.min(startIndex + this.pageSize, this.likedMusicItems.length);
+    const endIndex = Math.min(
+      startIndex + this.pageSize,
+      this.likedMusicItems.length
+    );
     return this.likedMusicItems.slice(startIndex, endIndex);
   }
 
@@ -94,22 +104,21 @@ export class HeroComponent {
   }
 
   playSong(url: string) {
-      const embedUrl = this.getEmbedUrl(url);
-      const dialogRef = this.dialog.open(MusicDialogComponent, {
-        data: { url: embedUrl },
-        width: '50%',
-        height: '55%'
-  
-      });
-  
-      dialogRef.afterClosed().subscribe(result => {
-        console.log('The dialog was closed');
-      });
-    }
-  
-    private getEmbedUrl(url: string): SafeResourceUrl {
-      const videoId = url.split('v=')[1];
-      const embedUrl = `https://www.youtube.com/embed/${videoId}`;
-      return this.sanitizer.bypassSecurityTrustResourceUrl(embedUrl);
-    }
+    const embedUrl = this.getEmbedUrl(url);
+    const dialogRef = this.dialog.open(MusicDialogComponent, {
+      data: { url: embedUrl },
+      width: '50%',
+      height: '85%',
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+    });
+  }
+
+  private getEmbedUrl(url: string): SafeResourceUrl {
+    const videoId = url.split('v=')[1];
+    const embedUrl = `https://www.youtube.com/embed/${videoId}`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(embedUrl);
+  }
 }
