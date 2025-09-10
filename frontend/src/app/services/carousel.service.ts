9d89a60dfdd61b4dfbc6a3818e938d5a052a7074
kvnq-poza <kvnqpoza@gmail.com>
2025-09-10 15:54:15 +0100

    Update carousel service

--- frontend/src/app/services/carousel.service.ts
+++ frontend/src/app/services/carousel.service.ts
@@ -2,33 +2,22 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { MusicCheckerService } from './music-checker.service';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class CarouselService {
   private carouselItemsSubject = new BehaviorSubject<any[]>([]);
   public carouselItems$ = this.carouselItemsSubject.asObservable();
 
-  constructor(private musicService: MusicCheckerService) {
-    this.loadCarouselItems();
-  }
-
-  // Load music from the grid
+  constructor(private musicService: MusicCheckerService) {}
 
   loadCarouselItems() {
-    // Use getAllMusics with limit of 6 for carousel
     this.musicService.getMusics(6, 7).subscribe({
       next: (items) => {
-        // Extract the data array from the response if it's paginated
         const musicItems = items.data || items;
         this.carouselItemsSubject.next(musicItems);
       },
       error: () => {
-        // Fallback to last checked musics if getAllMusics fails
-        this.musicService.getLastChecked(6).subscribe({
-          next: (items) => {
-            this.carouselItemsSubject.next(items);
-          },
+        this.musicService.getLastChecked(8).subscribe({
+          next: (items) => this.carouselItemsSubject.next(items),
         });
       },
     });
