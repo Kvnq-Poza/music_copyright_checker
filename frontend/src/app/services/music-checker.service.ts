26d540d18687d290d2bb15ae94c7d7d937f72b81
kvnq-poza <kvnqpoza@gmail.com>
2025-10-15 20:08:14 +0100

    refactor: replace YouTube API with TubePilot API integration
    - Remove unused getVidsByName, getVidsById, getLicense methods
    - Add checkCopyright method for copyright verification
    - Add searchMusic method with advanced filters
    - Add getGenres, getMoodsApi, getChannels methods

--- frontend/src/app/services/music-checker.service.ts
+++ frontend/src/app/services/music-checker.service.ts
@@ -5,42 +5,48 @@ import { resourceLimits } from 'worker_threads';
 import { environment } from '../../enviroments/enviroment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MusicCheckerService {
   apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient ) { }
+  constructor(private http: HttpClient) {}
 
-  getVidsByName(search_query: string): Observable<any> {
-    // return this.http.get<any>(`http://localhost:3000/youtube/video-by-name?video_name=${search_query}`)
-    return this.http.get<any>(`${this.apiUrl}/youtube/video-by-name?video_name=${search_query}`)
-  }
-  getVidsById(videoId: string): Observable<any> {
-    // return this.http.get<any>(`http://localhost:3000/youtube/video?video_id=${videoId}`)
-    return this.http.get<any>(`${this.apiUrl}/youtube/video?video_id=${videoId}`)
-  }
-
-  getLicense(videoId: string): Observable<any> {
-    // return this.http.get<any>(`http://localhost:3000/youtube/video?video_id=${videoId}`)
-    return this.http.get<any>(`${this.apiUrl}/youtube/license?video_id=${videoId}`)
+  checkCopyright(youtubeUrl: string): Observable<any> {
+    // return this.http.post<any>(
+    //   `http://localhost:3000/youtube/copyright-check`,
+    //   {
+    //     youtube_url: youtubeUrl,
+    //   }
+    // );
+    return this.http.post<any>(`${this.apiUrl}/youtube/copyright-check`, {
+      youtube_url: youtubeUrl,
+    });
   }
 
   likeVideo(videoId: string): Observable<any> {
-    // return this.http.post<any>(`http://localhost:3000/youtube/music`, {video_id: videoId})
-    return this.http.post<any>(`${this.apiUrl}/youtube/music`, {video_id: videoId})
+    // return this.http.post<any>(`http://localhost:3000/youtube/music`, {
+    //   video_id: videoId,
+    // });
+    return this.http.post<any>(`${this.apiUrl}/youtube/music`, {
+      video_id: videoId,
+    });
   }
   getMusics(limit: number, page: number): Observable<any> {
-    // return this.http.get<any>(`http://localhost:3000/music/all`, { params: { limit: limit.toString(), page: page.toString() } })
-    return this.http.get<any>(`${this.apiUrl}/music/all`, { params: { limit: limit.toString(), page: page.toString() } })
+    // return this.http.get<any>(`http://localhost:3000/music/all`, {
+    //   params: { limit: limit.toString(), page: page.toString() },
+    // });
+    return this.http.get<any>(`${this.apiUrl}/music/all`, {
+      params: { limit: limit.toString(), page: page.toString() },
+    });
   }
   createMusic(data: any): Observable<any> {
     // Retrieve the token from localStorage
     const token = localStorage.getItem('token');
-  
+
     // Set up the headers with the Authorization token
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-  
+
     // Make the POST request with the headers
     // return this.http.post<any>('http://localhost:3000/music', data, { headers });
     return this.http.post<any>(`${this.apiUrl}/music`, data, { headers });
@@ -49,19 +55,44 @@ export class MusicCheckerService {
     const token = localStorage.getItem('token');
 
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    // return this.http.get<any>(`http://localhost:3000/music/user`, { params: { limit: limit.toString(), page: page.toString() }, headers })
-    return this.http.get<any>(`${this.apiUrl}/music/user`, { params: { limit: limit.toString(), page: page.toString() }, headers })
+    // return this.http.get<any>(`http://localhost:3000/music/user`, {
+    //   params: { limit: limit.toString(), page: page.toString() },
+    //   headers,
+    // });
+    return this.http.get<any>(`${this.apiUrl}/music/user`, {
+      params: { limit: limit.toString(), page: page.toString() },
+      headers,
+    });
   }
 
   getTags(): Observable<any> {
-    // return this.http.get<any>(`http://localhost:3000/tags/top/10`)
-    return this.http.get<any>(`${this.apiUrl}/tags/top/10`)
+    // return this.http.get<any>(`http://localhost:3000/tags/top/10`);
+    return this.http.get<any>(`${this.apiUrl}/tags/top/10`);
   }
 
   // get last checked musics
   getLastChecked(n: number): Observable<any> {
-    // return this.http.get<any>(`http://localhost:3000/music/last-checked/${n}`)
-    return this.http.get<any>(`${this.apiUrl}/music/last-checked/${n}`)
+    // return this.http.get<any>(`http://localhost:3000/music/last-checked/${n}`);
+    return this.http.get<any>(`${this.apiUrl}/music/last-checked/${n}`);
   }
-  
+
+  searchMusic(params: any): Observable<any> {
+    // return this.http.post<any>(`http://localhost:3000/youtube/music-search`, params);
+    return this.http.post<any>(`${this.apiUrl}/youtube/music-search`, params);
+  }
+
+  getGenres(): Observable<any> {
+    // return this.http.get<any>(`http://localhost:3000/youtube/genres`);
+    return this.http.get<any>(`${this.apiUrl}/youtube/genres`);
+  }
+
+  getMoodsApi(): Observable<any> {
+    // return this.http.get<any>(`http://localhost:3000/youtube/moods`);
+    return this.http.get<any>(`${this.apiUrl}/youtube/moods`);
+  }
+
+  getChannels(): Observable<any> {
+    // return this.http.get<any>(`http://localhost:3000/youtube/channels`);
+    return this.http.get<any>(`${this.apiUrl}/youtube/channels`);
+  }
 }
